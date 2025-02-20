a17abfd5a0ae7d92ae8ca5a78edb7a0d75a8e433
SamuFisco <samuel.melian.c@gmail.com>
2025-02-20 12:05:00 +0000

    20/02/25 12:05
    - Menú Game over, arreglado el botón que te manda a la scena menú.

--- Assets/Scripts/UIManager/GameOverUI.cs
+++ Assets/Scripts/UIManager/GameOverUI.cs
@@ -1,31 +1,21 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // Necesario para cambiar escenas
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
-    public EnemyVision enemyVision;
-    public string menuSceneName = "Menu"; // Nombre de la escena del Menú
+    public string menuSceneName = "Menu"; // Nombre de la escena del menú
 
     // Método para reiniciar la escena actual desde el inicio
     public void ReiniciarJuego()
     {
-        Time.timeScale = 1; // Asegurar que el juego no esté pausado
+        Time.timeScale = 1; // Restaurar el tiempo de juego
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Recarga la escena actual
     }
 
     // Método para ir al menú principal
     public void IrAlMenu()
     {
-        Time.timeScale = 1; // Asegurar que el tiempo se restaure
-        SceneManager.LoadScene(menuSceneName); // Cargar la escena del Menú
-    }
-
-    // Método para activar la bandera de reinicio en EnemyVision
-    public void ActivarReinicioDesdeUI()
-    {
-        if (enemyVision != null)
-        {
-            enemyVision.ActivarReinicio();
-        }
+        Time.timeScale = 1; // Restaurar el tiempo en caso de que esté pausado
+        SceneManager.LoadScene(menuSceneName); // Cargar la escena del menú principal
     }
 }
